92f153d026bfd9539e5cb1234bec2c645fbc8324
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2022-12-28 12:32:20 +0600

    Add files via upload

---
+++ array 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+
+{
+    int ara[8]={10,20,30,40,50,60,70,80};
+    printf("First element:%d\n",ara[0]);
+    printf("Fifth element:%d\n",ara[4]);
+
+    return 0;
+
+}
