2cf50b4cecea9c96b00aef3035f70c1931ef9940
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2023-01-12 09:46:48 +0600

    Add files via upload

---
+++ Address.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+
+{
+    printf(" \a");
+    printf(" MAHIR FAISAL\n");
+    printf("  BSSE-13th\n");
+    printf("  ROLL-1316\n");
+    printf("  IIT,UNIVERSITY OF DHAKA\n");
+    return 0;
+}
