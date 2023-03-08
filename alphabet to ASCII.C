7990eb93e167ec2cd5dd1f3c376ea6462c003614
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2023-03-08 23:20:01 +0600

    Add files via upload

---
+++ alphabet to ASCII.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+
+{
+    char ch;
+      for(ch='A';ch<='Z';ch++)
+       printf("The character is %c\n");
+
+      for(ch='a';ch<='z';ch++)
+       printf("The character is %c\n");
+
+     return 0;
+}
