eccf3ef30b17861012add3cb5515872a9a1015f0
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2023-03-08 23:25:30 +0600

    Add files via upload

---
+++ for 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+
+{
+     int num;
+     for(num=1;num<=10;num++)
+        {
+          printf("%d ",num);
+        }
+        printf("terminating");
+    return 0;
+}
