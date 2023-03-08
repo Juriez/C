172d36db8e6cd2cf02d8c28bead636bc74cc8cb2
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2023-03-08 23:21:29 +0600

    Add files via upload

---
+++ beginning1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    char c='A';
+    bool d=true;
+    cin>>a;
+    cout << a << " " << c << endl <<d<<endl;
+    d=5;   /*for all value except 0,the varible bool always print 1*/
+    cout<<d<<endl;
+    d=0;
+    cout<<d<<endl;
+    cout << (int)c<< endl;/*type cast*/
+
+}
